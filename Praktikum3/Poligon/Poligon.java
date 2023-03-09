9c604007860d1d0fa11c20e2a6c47e04a77240ff
divaaynd <100470946+divaaynd@users.noreply.github.com>
2023-03-09 14:52:17 +0700

    Add files via upload

---
+++ Praktikum3/Poligon/Poligon.java
@@ -0,0 +1,14 @@
+/* File     : Poligon.java 
+* Tanggal   : 08/03/2023
+* Penulis   : Diva Ayunda Najwina
+* Deskripsi : Representasi dasar dara objek poligon (segi banyak)
+*/
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
