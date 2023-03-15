b74312ba8d0976c85b300cedae3894894b016076
divaaynd <100470946+divaaynd@users.noreply.github.com>
2023-03-15 20:58:45 +0700

    Add files via upload

---
+++ Praktikum4/MKubus/MKubus.java
@@ -0,0 +1,21 @@
+/**
+* File      : MKubus.java 
+* Tanggal   : 15/03/2023
+* Penulis   : Diva Ayunda Najwina
+* Deskripsi : Main dari kubus
+*/
+
+package org.main;
+
+import org.bangundatar.BujurSangkar;
+import org.bangunruang.Kubus;
+
+public class MKubus {
+    public static void main(String[] args) {
+              BujurSangkar bujurSangkar = new BujurSangkar(4);
+              Kubus kubus = new Kubus (bujurSangkar);
+
+              // TODO
+             // Print luas dan volume kubus
+        }
+}
