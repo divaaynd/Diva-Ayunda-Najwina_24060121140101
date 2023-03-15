480525411efefae18e4dd5e4ebac5bc4281951f2
divaaynd <100470946+divaaynd@users.noreply.github.com>
2023-03-15 20:29:03 +0700

    Update MOperasiTitik.java

--- Praktikum2/MOperasiTitik.java
+++ Praktikum2/MOperasiTitik.java
@@ -1,5 +1,5 @@
 // MOperasiTitik.java 01/03/22
-// Penulis : Rizky Akhmad Fahreza
+// Penulis : Diva Ayunda Najwina
 // Deskripsi : Kelas yang berisi program utama yang memanfaatkan kelas Operasi Titik
 
 
