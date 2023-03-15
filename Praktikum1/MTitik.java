9784989534f32a7b73c6079d002be1ae6ffcad1f
divaaynd <100470946+divaaynd@users.noreply.github.com>
2023-03-15 20:30:07 +0700

    Update MTitik.java

--- Praktikum1/MTitik.java
+++ Praktikum1/MTitik.java
@@ -1,5 +1,5 @@
 // MTitik.java 22/02/23
-// Penulis : Rizky Akhmad Fahreza
+// Penulis : Diva Ayunda Najwina
 // Deskripsi : Kelas yang berisi program utama yang memanfaatkan kelas titik
 
 class MTitik{
