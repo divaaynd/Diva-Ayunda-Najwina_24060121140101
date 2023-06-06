649b5a99898cddd6d85a725d8a1200eda074b15e
divaaynd <100470946+divaaynd@users.noreply.github.com>
2023-06-06 20:22:14 +0700

    Add files via upload

---
+++ Praktikum10/LambdaList.java
@@ -0,0 +1,20 @@
+/**
+ * File      : Lambdalist.java		31/05/23
+ * Penulis   : Diva Ayunda Najwina / 24060121140101
+ * Deskripsi : Implementasi lambda pada list, digunakan sebagai parameter pada method.
+ *
+ */
+
+import java.util.ArrayList;
+
+public class LambdaList {
+    public static void main(String[] args) {
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add("Varrel");
+        mahasiswaList.add("Inong");
+        mahasiswaList.add("Erlan");
+        mahasiswaList.add("Durapi");
+
+        //lambda digunakan sebagai parameter pada method
+        mahasiswaList.forEach((nama) -> System.out.println(nama));
+    }
